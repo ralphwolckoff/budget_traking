3ca66dad316eff958d1ac3db14022da228ca2f54
landry-ralph <tchouambiaaudrey@gmail.com>
2026-08-28 12:46:14 +0100

    fix build2

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/tailwind.css'
-import './styles/index.css'
-import './styles/responsive.css'
-import App from './App'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles/tailwind.css";
+import "./styles/index.css";
+import App from "./App";
 
-createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <App />
-    </StrictMode>
-)
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
