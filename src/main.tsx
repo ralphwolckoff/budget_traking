f433dfaf7145a22c5de144095636f5d03937ee38
landry-ralph <tchouambiaaudrey@gmail.com>
2026-08-28 12:00:38 +0100

    fix build

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/tailwind.css'
 import './styles/index.css'
-import './styles/selection.css'
 import './styles/responsive.css'
 import App from './App'
 
