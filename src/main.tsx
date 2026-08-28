37ab9c874da8de648b53060a19f4bd8e912d759f
Audrey <167112263+ralphwolckoff@users.noreply.github.com>
2026-08-28 12:12:41 +0100

    change seletion casse

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/tailwind.css'
 import './styles/index.css'
-import './styles/responsive.css'
+import './styles/Selection.css'
+import './styles/Responsive.css'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
